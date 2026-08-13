2d502bcf846661eddbfeb1e9de04802517968e3d
infaix <infaix@users.noreply.github.com>
2026-08-13 10:42:48 +1000

    Hotfix:v4 cloudflare deploy

---
+++ src/worker.ts
@@ -0,0 +1,18 @@
+export default {
+  async fetch(request: Request, env: any, ctx: any): Promise<Response> {
+    const url = new URL(request.url)
+    const pathName = url.pathname === "/" ? "/index.html" : url.pathname
+
+    try {
+      const response = await fetch(`out${pathName}`)
+      const html = await response.text()
+      return new Response(html, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+        },
+      })
+    } catch {
+      return new Response("Not found", { status: 404 })
+    }
+  },
+}
